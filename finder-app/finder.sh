5216a9e5c5afd80a43522d1a32a84ad0ac83c5fc
sanishkharade <sanishkharade@gmail.com>
2022-01-17 19:42:46 -0700

    Assignment 1 commit

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-filesdir=$1
-searchstr=$2
-
-#echo Path is $filesdir
-#echo Search string is $searchstr
-
-#echo
+: '
+ @file    		:   finder.sh
+ @brief   		:   Script to find all occurences of a particular string in a directory
+ 
+ @author  		:   Sanish Kharade
+ @date    		:   January 17, 2022
+  
+ @references    :   find - https://devconnected.com/how-to-count-files-in-directory-on-linux/
+					grep - https://stackoverflow.com/questions/6135065/how-to-count-occurrences-of-a-word-in-all-the-files-of-a-directory/6135712
+'
 
 # Check if 2 arguments are passed
 if [ $# -lt 2 ] 
@@ -16,20 +19,33 @@ then
 	echo "Order of arguments should be -"
 	echo "	1) Path of Directory"
 	echo "	2) String to be searched"
+	echo
 	exit 1
 fi
 
+# Store the recieved arguments in varaibles
+filesdir=$1
+searchstr=$2
+
 # Check if path provided is a directory
-if [ -d $filesdir ]
+if [ ! -d $filesdir ]
 then
-	echo "$filesdir is a directory"
-else
 	echo "$filesdir is not a directory"
 	exit 1
 fi
 
-X=$( ls $filesdir | wc -l )
-#grep -rnw $filesdir -e $searchstr
-Y=$( grep -roh $searchstr $filesdir | wc -w)
+# Find the number of files in the directory 
+X=$( find $filesdir -type f | wc -l )
+
+# Find the occurences of string in the directory
+Y=$( grep -roh $searchstr $filesdir | wc -l)
+
+#Print the output
 echo The number of files are $X and the number of matching lines are $Y
+
+
+
+
+
+
 
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-writefile=$1
-writestr=$2
-
-#echo Path is $writefile
-#echo String is $writestr
-
-#echo
+: '
+ @file    		:   writer.sh
+ @brief   		:   Script to write a particular string to a file
+ 
+ @author  		:   Sanish Kharade
+ @date    		:   January 17, 2022
+  
+ @references    :   get directory name - https://www.cyberciti.biz/faq/unix-get-directory-name-from-path-command/
+'
 
 # Check if 2 arguments are passed
 if [ $# -lt 2 ] 
@@ -19,21 +21,35 @@ then
 	exit 1
 fi
 
-echo $writestr > $writefile
-if [ -w $writefile ] && [ ! -d $writefile ]
+# Store the recieved arguments in varaibles
+writefile=$1
+writestr=$2
+
+# Get directory name from the path
+directoryname="${writefile%/*}"
+
+# Check if directory exists
+if [ ! -d $directoryname ]
 then
-	#echo "File written successfully"
+	echo "$directoryname doesn't exist. Creating the directory"
+	mkdir $directoryname
+fi	
+
+# Write the string to the file
+echo $writestr > $writefile
+
+: '
+Check if the file was written successfully.
+It could happen that the file exists but does not have write permission
+Hence -w is used to check that
+'
+if [ -w $writefile ]
+then
 	exit 0	
 else
 	echo "File could not be written"
 	exit 1
 fi
-#myvar=$( cat $writefile )
-#if [ myvar != $writestr ]
-#then 
-#	echo "File could not be written"
-#	exit 1
-#else
-#	echo "File written successfully"
-#	exit 0
-#fi
+
+
+
